9b28d9e126f43efbe8ebab01318c30b05a34575c
cqc-hub <1062170019@qq.com>
2020-04-24 22:32:26 +0800

    'optimize'

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router/index'
 import store from './store'
 import tost from './components/common/tost/index'
 import fastClick from'fastclick'
-
+import VueLazyload from 'vue-lazyload'
 Vue.config.productionTip = false
 
 import { Icon } from 'vant';
@@ -13,7 +13,9 @@ import { Image } from 'vant';
 Vue.use(Image);
 Vue.use(Icon);
 
-
+Vue.use(VueLazyload,{
+	loading:require('assets/img/common/lufei.jpg')
+})
 //安装tost插件
 Vue.use(tost)  //vue.use会执行install函数
 
